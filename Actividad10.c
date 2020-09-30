1954cd350d7fc02a663abd23f167a4241fb67ce4
Daniel Rivera <71423618+dnrivera@users.noreply.github.com>
2020-09-30 18:24:57 -0300

    Update Actividad10.c
    Añadida consigna

--- Actividad10.c
+++ Actividad10.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+El ministerio de Salud de la Nación tiene puntos de reparto de vacunas que
+se pretende funcionen de la siguiente manera. Cada día, empezar con 1000
+vacunas disponibles y a través de un programa que controla las entregas
+avisar si el inventario baja de 200 unidades. Realizar el diagrama de flujo.
+*/
+
 int main()
 {
     int unidades = 1000;
